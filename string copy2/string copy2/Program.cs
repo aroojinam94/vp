e983eda608aed8d4f5efef0d66981377aebe473d
aroojinam94 <aroojinam1994@gmail.com>
2016-03-18 23:36:29 +0500

    string copyto

---
+++ string copy2/string copy2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace string_copy2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str="Arooj";
+             Char[]arr=new char[5];
+                str.CopyTo(3,arr,0,3);
+                Console.WriteLine(arr);
+                string  str1 = "another testing";
+                Console.WriteLine("str1");
+                Console.ReadLine();
+        }
+    }
+}
