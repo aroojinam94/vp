2f6f17c9fdb2927ecf2cbaf428c35e6c46f584cd
aroojinam94 <aroojinam1994@gmail.com>
2016-03-18 23:42:11 +0500

    trim

---
+++ trim/trim/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace trim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Arooj inam abbasi  ");
+            string firstName = Console.ReadLine();
+            firstName.Trim('a','j');
+            Console.WriteLine(firstName);
+            Console.ReadLine();
+
+           
+                
+        }
+    }
+}
